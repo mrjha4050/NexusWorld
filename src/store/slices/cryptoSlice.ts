b128b8e8125d1df5d856afca9c711e306fc6545a
mrjha4050 <mrjha4050@gmail.com>
2025-04-02 02:59:33 +0530

    Remove unused imports

--- src/store/slices/cryptoSlice.ts
+++ src/store/slices/cryptoSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { CryptoState, CryptoData, NewsArticle } from '@/types';
+import { CryptoState } from '@/types';
 
 const initialState: CryptoState = {
   data: {},
